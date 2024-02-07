5723218fe006f00e69b1a88bbe7bdadac04a2b25
godfish13 <63995325+godfish13@users.noreply.github.com>
2024-02-07 19:19:54 +0900

    Uils update
    FindChild -> FindComponentInChild 로 메소드 이름 직관적으로 변경

--- Assets/Scripts/Utility/Utils.cs
+++ Assets/Scripts/Utility/Utils.cs
@@ -13,16 +13,16 @@ public class Utils
         return Component;
     }
 
-    public static GameObject FindChild(GameObject parent, string Childname = null, bool recursive = false)   // UI항목 말고 GameObject도 enum과 연동하기 위해
+    public static GameObject FindComponentinChild(GameObject parent, string childname = null, bool recursive = false)   // UI항목 말고 GameObject도 enum과 연동하기 위해
     {                                                                                               // 일반화 없는 GameObject버전도 만들어둠
-        Transform transform =  FindChild<Transform>(parent, Childname, recursive);      // GameObject는 Monobehavior, ... 을 상속받지 않은 것이라 따로
+        Transform transform = FindComponentinChild<Transform>(parent, childname, recursive);      // GameObject는 Monobehavior, ... 을 상속받지 않은 것이라 따로
         if (transform == null)                                                 // 구현하라고 오류뜸 그러므로 일반화아닌버전 제작
             return null;
 
         return transform.gameObject;
     }
 
-    public static T FindChild<T>(GameObject parent, string Childname = null, bool recursive = false) where T : UnityEngine.Object
+    public static T FindComponentinChild<T>(GameObject parent, string childname = null, bool recursive = false) where T : UnityEngine.Object
     {                                                               //recursive : child항목 아래의 child까지 탐색여부 체크
         if (parent == null)
             return null;
@@ -32,7 +32,7 @@ public class Utils
             for(int i = 0; i < parent.transform.childCount; i++)
             {
                 Transform transform = parent.transform.GetChild(i);
-                if(string.IsNullOrEmpty(Childname) || transform.name == Childname)
+                if(string.IsNullOrEmpty(childname) || transform.name == childname)  // childname 따로 지정 안했으면 T에 해당되는 것 전부 return
                 {
                     T component = transform.GetComponent<T>();
                     if(component != null)
@@ -44,7 +44,7 @@ public class Utils
         {
             foreach (T component in parent.GetComponentsInChildren<T>())
             {
-                if (string.IsNullOrEmpty(Childname) || component.name == Childname)
+                if (string.IsNullOrEmpty(childname) || component.name == childname)
                     return component;
             }
         }
